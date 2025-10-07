a8e666f2fd069a14c181874f0b6f32b5d2b17b5f
MOUCHARD YANNIS <ymouchar@peda.pun.univ-poitiers.fr>
2025-10-07 17:04:50 +0200

    fix(tests): 🐛 new pimp & correction bug

--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -1,6 +1,13 @@
 import { useLoaderData } from 'react-router-dom'
 import CharactersList from '../components/CharactersList'
 import NumberOfCharacters from '../components/NumberOfCharacters'
+import { getCharacters } from '../api/characters-api'
+
+// Fonction loader pour React Router
+export async function loader() {
+  const characters = await getCharacters()
+  return characters
+}
 
 function CharactersPage() {
   const characters = useLoaderData()
